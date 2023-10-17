d3f8fcfb311e6118dadffe25917b63c0cf5b95d6
Shayln Beckstead <shaylnmb22@gmail.com>
2023-10-16 20:35:20 -0600

    the start and game over text stuff, but it doesnt work

--- unit3/UFO defense force game/Assets/scripts/collisionDetect.cs
+++ unit3/UFO defense force game/Assets/scripts/collisionDetect.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class collisionDetect : MonoBehaviour
 {
+    public ScoreManager scoreManager;
+    public int scoreToGive;
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
     void OnTriggerEnter(Collider other)
     {
+        scoreManager.IncreaseScore(scoreToGive);
         if(other.gameObject.tag == "bullet")
         {
             Destroy(gameObject); //destroy this game object
---
+++ unit3/UFO defense force game/Assets/scripts/mainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public int sceneToLoad;
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneToLoad); //which scene to load
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game!");
+    }
+}
